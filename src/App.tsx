e690559872c2f36419221eaa95fa452b0a391397
Alexandr Dylevich <dylevich220885@gmail.com>
2023-03-01 23:59:40 +0300

    added the use of useEffect and localStorage so that the counter value is saved after the page is reloaded.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 import {Button} from './components/Button';
 import {Monitor} from './components/Monitor';
@@ -10,25 +10,46 @@ function App() {
 
     const [count, setCount] = useState<number>(startValue)
 
-    function incCount() {
-        if (count < maxValue){
-            //const newCount = count + 1 //можно в setCount передать newCount, а можно сразу передать count+1
+    //используем useEffect и localStorage, чтобы после перезагрузки странички сохранялось значение счетчика
+    useEffect(()=>{getFromLocalStorageHandler()}, []) //единожды, только при первой отрисовке странички, забираем из localStorage хранящееся там значение и помещаем его в useState в качестве инициализационного значения
 
-            setCount(count + 1)
-            //setCount(count => count+1) //можно также передать функцию, которая принимает старое значение и возвращает новое значение count
+    useEffect(()=>setToLocalStorageHandler(), [count])//при каждом изменении count (в useState), помещаем в localStorage это значение (count)
+
+    //колбэк для юзэффекта
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('count', JSON.stringify(count))
+    }
+    //колбэк для юзэффекта
+    const getFromLocalStorageHandler = () => {
+        let valueAsString = localStorage.getItem('count')
+        if(valueAsString){
+            let newCount = JSON.parse(valueAsString)
+            setCount(newCount)
+            //return newCount //если убрать setCount(newCount), а просто возвращать newCount, то эту функцию можно сразу поместить в useState в качестве инициализационного значения
         }
     }
 
-    function resetCount() {
+
+
+    const incCount = () => {
+        if (count < maxValue) {
+            //const newCount = count + 1 //можно в setCount передать newCount, а можно сразу передать count+1
+            setCount(count + 1)
+            //setCount(count => count+1) //можно также передать функцию, которая принимает старое значение и возвращает новое значение count
+
+        }
+    }
+
+    const resetCount = () => {
         setCount(startValue)
     }
 
-    const isDisableInc:boolean = count === maxValue
-    const isDisableReset:boolean = count === startValue
+    const isDisableInc: boolean = count === maxValue
+    const isDisableReset: boolean = count === startValue
 
     return (
         <div className={'App'}>
-            <div className={'monitor' + (count>=5?' red':'')}>
+            <div className={'monitor' + (count >= 5 ? ' red' : '')}>
                 <Monitor count={count}/>
             </div>
             <div className={'buttons'}>
