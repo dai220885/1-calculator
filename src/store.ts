84e89f24a3d47234b0c46cdf937da7eefde90cd1
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-24 20:07:27 +0300

    Добавлена типизация, описан useAppDispatch

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
-import {combineReducers, legacy_createStore as createStore} from 'redux';
-import {startValueReducer} from './reducers/startValueReducer';
-import {maxValueReducer} from './reducers/maxValueReducer';
-import {countReducer} from './reducers/countReducer';
-import {settingsReducer} from './reducers/settingsReducer';
-import {commonReducer} from './reducers/commonReducer';
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux';
+import {SetStartValueActionType, startValueReducer} from 'reducers/startValueReducer';
+import {maxValueReducer, SetMaxValueActionType} from 'reducers/maxValueReducer';
+import {countReducer, countReducerActionsType} from 'reducers/countReducer';
+import {settingsReducer, settingsReducerActionsType} from 'reducers/settingsReducer';
+import {commonReducer} from 'reducers/commonReducer';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import {useDispatch} from 'react-redux';
 
 
 //используем четыре редьюсера (по одному для каждого из первоначально используемых setState-ов)
@@ -23,9 +25,21 @@ const rootReducer2 = combineReducers(
     }
 )
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const useAppDispatch = () => useDispatch<AppDispatchType>()
+
+//types:
+export type AppRootStateType = ReturnType<typeof rootReducer>
+type AppStoreType = typeof store
+export type AppDispatchType = ThunkDispatch <AppRootStateType, unknown, AppActionsType>
+type AppActionsType =
+  | countReducerActionsType
+  | settingsReducerActionsType
+  | SetMaxValueActionType
+  | SetStartValueActionType
+
+
 
 // @ts-ignore
 window.store = store
