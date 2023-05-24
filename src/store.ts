d21d4b2866d0c60da680110df2db7eb8f1798598
Alexandr Dylevich <dylevich220885@gmail.com>
2023-05-24 21:04:31 +0300

    при создании стора в createStore вторым параметром передали функцию loadState(), которая вернет стейт, полученный из localStorage. в store.subscribe передал колбэк, который запустит saveState()

--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,7 @@ import {settingsReducer, settingsReducerActionsType} from 'reducers/settingsRedu
 import {commonReducer} from 'reducers/commonReducer';
 import thunk, {ThunkDispatch} from 'redux-thunk';
 import {useDispatch} from 'react-redux';
+import {loadState, saveState} from 'utils/localStorageUtils';
 
 
 //используем четыре редьюсера (по одному для каждого из первоначально используемых setState-ов)
@@ -18,16 +19,28 @@ const rootReducer = combineReducers(
 
     }
 )
-//здесь используем один редьюсер, обрабатывающий стейт типа StateType (объект со свойствами startValue, maxValue, count, isSettingsVisible
+
 const rootReducer2 = combineReducers(
     {
         calculator: commonReducer
     }
 )
 
-
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+//вторым параметром передаем функцию, которая вернет стейт, полученный из localStorage
+export const store = createStore(rootReducer, loadState(), applyMiddleware(thunk))
 export const useAppDispatch = () => useDispatch<AppDispatchType>()
+
+//в store.subscribe передали колбэк, который запустит saveState(), в aveState() в качестве параметра придет объект типа AppRootStateType, каждое свойство которого получено с помощью store.getState()
+store.subscribe(() => {
+  //localStorage.setItem('appState', JSON.stringify(store.getState()))
+  saveState({
+    startValue: store.getState().startValue,
+    maxValue: store.getState().maxValue,
+    count: store.getState().count,
+    isSettingsVisible: store.getState().isSettingsVisible,
+  });
+  //localStorage.setItem('currentCount', JSON.stringify(store.getState().count.currentValue))
+})
 
 //types:
 export type AppRootStateType = ReturnType<typeof rootReducer>
